7a71b028a274e982c87d5251193590c0fd45af90
MahmudulHasan <autul.aust@gmail.com>
2025-02-12 23:17:05 +0600

    Implemented page object for checkout page.

---
+++ page-objects/ProductCheckoutPage.ts
@@ -0,0 +1,85 @@
+import { Page, Locator, expect } from '@playwright/test';
+
+export class ProductCheckoutPage {
+    private page: Page;
+    private cartButton: Locator;
+    private cartItem: Locator;
+    private checkoutButton1: Locator;
+    private proceedText: Locator;
+    private checkoutButton2: Locator;
+    private addressInput: Locator;
+    private cityInput: Locator;
+    private stateInput: Locator;
+    private countryInput: Locator;
+    private postcodeInput: Locator;
+    private proceedButton: Locator;
+    private paymentMethodInput: Locator;
+    private paymentFinishButton: Locator;
+    private successMessage: Locator;
+    private invoiceNumber: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.cartButton = page.locator('[data-test="nav-cart"]');
+        this.cartItem = page.locator('[data-test="product-title"]');
+        this.checkoutButton1 = page.locator('[data-test="proceed-1"]');
+        this.proceedText = page.getByText('Hello Jane Doe, you are already logged in. You can proceed to checkout.');
+        this.checkoutButton2 = page.locator('[data-test="proceed-2"]');
+        this.addressInput = page.locator('[data-test="address"]');
+        this.cityInput = page.locator('[data-test="city"]');
+        this.stateInput = page.locator('[data-test="state"]');
+        this.countryInput = page.locator('[data-test="country"]');
+        this.postcodeInput = page.locator('[data-test="postcode"]');
+        this.proceedButton = page.locator('[data-test="proceed-3"]');
+        this.paymentMethodInput = page.locator('[data-test="payment-method"]');
+        this.paymentFinishButton = page.locator('[data-test="finish"]');
+        this.successMessage = page.locator('.help-block');
+        this.invoiceNumber = page.locator('[id="order-confirmation"] span');
+    }
+
+    async proceedToCheckout(productName: string) {
+        await this.cartButton.click();
+        await expect(this.cartItem).toHaveText(productName);
+        await this.page.reload({ waitUntil: 'load' });
+        await this.checkoutButton1.click();
+        await expect(this.proceedText).toBeVisible();
+        await this.checkoutButton2.click();
+    }
+
+    async enterBillingAddress(address: string, city: string, country: string, postcode: string) {
+        await this.addressInput.clear();
+        await this.addressInput.pressSequentially(address, { delay: 100 });
+        await this.cityInput.clear();
+        await this.cityInput.pressSequentially(city, { delay: 100 });
+        await this.stateInput.clear();
+        await this.stateInput.pressSequentially(city, { delay: 100 });
+        await this.countryInput.clear();
+        await this.countryInput.pressSequentially(country, { delay: 100 });
+        await this.postcodeInput.clear();
+        await this.postcodeInput.pressSequentially(postcode, { delay: 100 });
+        await this.proceedButton.click();
+    }
+
+    async selectPaymentMethod(paymentMethod: string) {
+        await this.paymentMethodInput.selectOption({ label: paymentMethod });
+        await this.paymentFinishButton.click();
+    }
+
+    async verifySuccessMessage(expectedMessage: string) {
+        await expect(this.successMessage).toHaveText(expectedMessage);
+    }
+
+    async confirmOrder() {
+        await this.paymentFinishButton.click();
+    }
+
+    async getInvoiceNumber() {
+        await this.invoiceNumber.waitFor();
+        await expect(this.invoiceNumber).toBeVisible({ timeout: 60000 });
+        const invoiceText = await this.invoiceNumber.textContent();
+        process.env.invoiceNumberShared = invoiceText || '';
+        console.log("Invoice Number: ", invoiceText);
+        return invoiceText;
+    }
+}
+module.exports = {ProductCheckoutPage};
--- step-definitions/ProductCheckoutStep.ts
+++ step-definitions/ProductCheckoutStep.ts
@@ -1,85 +1,36 @@
 import { When, Then, setDefaultTimeout } from '@cucumber/cucumber';
-import { expect } from '@playwright/test';
 import { pageFixture } from '../hooks/pageFixtures';
-
+import { ProductCheckoutPage } from '../page-objects/ProductCheckoutPage';
 
 setDefaultTimeout(60000);
 
 When('the user proceeds to checkout for the product {string}', async (productName) => {
-    const cartBUtton = await pageFixture.page.locator('[data-test="nav-cart"]');
-    await cartBUtton.click();
-    const cartItem = await pageFixture.page.locator('[data-test="product-title"]');
-    await expect(cartItem).toHaveText("Court Hammer");
-    await pageFixture.page.reload({waitUntil:'load'});
-    const checkoutButton1 = await pageFixture.page.locator('[data-test="proceed-1"]');
-    await checkoutButton1.click();
-    const proceedText = await pageFixture.page.getByText('Hello Jane Doe, you are already logged in. You can proceed to checkout.');
-    await expect(proceedText).toBeVisible();
-    const checkoutButton2 = await pageFixture.page.locator('[data-test="proceed-2"]'); 
-    await checkoutButton2.click();
+    const productCheckoutPage = new ProductCheckoutPage(pageFixture.page);
+    await productCheckoutPage.proceedToCheckout(productName);
 });
 
-When('the user enters the Billing Address {string}, {string}, {string}, {string}', async (address, city, country, postcode) => {
-    //await pageFixture.page.pause();
-    const addressInput = await pageFixture.page.locator('[data-test="address"]');
-    await addressInput.clear();
-    await addressInput.pressSequentially(address,{delay:100});
-    const cityInput = await pageFixture.page.locator('[data-test="city"]');
-    await cityInput.clear();
-    await cityInput.pressSequentially(city,{delay:100});
-    const stateInput = await pageFixture.page.locator('[data-test="state"]');
-    await stateInput.clear();
-    await stateInput.pressSequentially(city,{delay:100});
-    const countryInput = await pageFixture.page.locator('[data-test="country"]');
-    await countryInput.clear();
-    await countryInput.pressSequentially(country,{delay:100});
-    const postcodeInput = await pageFixture.page.locator('[data-test="postcode"]');
-    await postcodeInput.clear();
-    await postcodeInput.pressSequentially(postcode,{delay:100});
-    const proceedButton = await pageFixture.page.locator('[data-test="proceed-3"]');
-    await proceedButton.click();
-});
-/*
-When('the user selects the payment method {string} and installment plan {string}', async(paymentMethod, installmentPlan)=>{
-    const paymentMethodInput = await pageFixture.page.locator('[data-test="payment-method"]');
-    await paymentMethodInput.selectOption({label: paymentMethod});
-    const installmentPlanInput = await pageFixture.page.locator('[data-test="monthly_installments"]');
-    await installmentPlanInput.selectOption({label: installmentPlan});
-    const paymentFinishButton = pageFixture.page.locator('[data-test="finish"]');
-    await paymentFinishButton.click();
-}); */
-When('the user selects the payment method {string}', async(paymentMethod)=>{
-    const paymentMethodInput = await pageFixture.page.locator('[data-test="payment-method"]');
-    await paymentMethodInput.selectOption({label: paymentMethod});
-    //const installmentPlanInput = await pageFixture.page.locator('[data-test="monthly_installments"]');
-    //await installmentPlanInput.selectOption({label: installmentPlan});
-    const paymentFinishButton = pageFixture.page.locator('[data-test="finish"]');
-    await paymentFinishButton.click();
+When('the user enters the Billing Address {string}, {string}, {string}, {string}', 
+    async (address, city, country, postcode) => {
+        const productCheckoutPage = new ProductCheckoutPage(pageFixture.page);
+        await productCheckoutPage.enterBillingAddress(address, city, country, postcode);
 });
 
-Then('the user should get a message {string}', async(paymentSuccessMessage) => {
-    const successMessage = await pageFixture.page.locator('.help-block');
-    await expect(successMessage).toHaveText(paymentSuccessMessage);
+When('the user selects the payment method {string}', async (paymentMethod) => {
+    const productCheckoutPage = new ProductCheckoutPage(pageFixture.page);
+    await productCheckoutPage.selectPaymentMethod(paymentMethod);
 });
 
-When('the user clicks confirm button', async() => {
-   // await pageFixture.page.waitForTimeout(3000);
-    //const paymentFinishButton = await pageFixture.page.locator('[data-test="finish"]');
-    //await expect(paymentFinishButton).toBeVisible();
-   // await paymentFinishButton.waitFor();
-   const paymentFinishButton = pageFixture.page.locator('[data-test="finish"]');
-    
-    //await paymentFinishButton.click({timeout: 60000});
-    //await pageFixture.page.keyboard.press('Enter');
-    //await pageFixture.page.pause();
-    await paymentFinishButton.click();
+Then('the user should get a message {string}', async (paymentSuccessMessage) => {
+    const productCheckoutPage = new ProductCheckoutPage(pageFixture.page);
+    await productCheckoutPage.verifySuccessMessage(paymentSuccessMessage);
 });
 
-Then('the user should get a invoice number', async function() {
-    const invoiceNumber = await pageFixture.page.locator('[id="order-confirmation"] span');
-    await invoiceNumber.waitFor();
-    await expect(invoiceNumber).toBeVisible({timeout: 60000});
-    console.log("Invoice Number: ", await invoiceNumber.textContent());
-    process.env.invoiceNumberShared = await invoiceNumber.textContent();
-    console.log("Invoice Number2: ", process.env.invoiceNumberShared);
+When('the user clicks confirm button', async () => {
+    const productCheckoutPage = new ProductCheckoutPage(pageFixture.page);
+    await productCheckoutPage.confirmOrder();
+});
+
+Then('the user should get an invoice number', async function () {
+    const productCheckoutPage = new ProductCheckoutPage(pageFixture.page);
+    await productCheckoutPage.getInvoiceNumber();
 });
